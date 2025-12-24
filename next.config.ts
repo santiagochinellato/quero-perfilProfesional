6abc4f81ca4e74e3b507902aeea4f48db17e61ed
santiagochinellato <santiagochinellato@gmail.com>
2025-12-24 14:29:50 +0000

    fixed next config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
+const isProd = process.env.NODE_ENV === "production" || process.env.GITHUB_ACTIONS === "true";
 
 const nextConfig: NextConfig = {
   output: "export",
